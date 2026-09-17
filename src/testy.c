82d193b06b475cedfaa62ba2ab6a5801f143e822
Introdo <263558585+Introdo@users.noreply.github.com>
2026-09-17 11:36:29 +0200

    BAZINGA

--- src/testy.c
+++ src/testy.c
@@ -2,12 +2,34 @@
 
 int main(void) 
 {
+	Vector2i playerPos = (Vector2i){ GetTuiDimensions().x / 2, GetTuiDimensions().y / 2 };
 	InitTui(60,TUI_DYNAMIC);
+	HideCursor();
 
 	while(true)
 	{
 		BeginFrame();
 		ClearTui(BLACK,TERMWHITE);
+		
+		if(IsKeyPressed('w')) 
+		{
+			playerPos.y -= 1;
+		}
+		if(IsKeyPressed('a')) 
+		{
+			playerPos.x -= 2;
+		}
+		if(IsKeyPressed('s')) 
+		{
+			playerPos.y += 1;
+		}
+		if(IsKeyPressed('d')) 
+		{
+			playerPos.x += 2;
+		}
+		
+		DrawChar("M",playerPos.x,playerPos.y,WHITE);
+		
 		EndFrame();
 	}
 
